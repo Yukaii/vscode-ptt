f99ed9b8a429b45b1c445df250f46c93d220ff15
Yukai Huang <yukaihuangtw@gmail.com>
2019-03-23 14:51:55 +0800

    Properly handle ptt-client initialization

--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 (global as any).WebSocket = require('ws');
-
-const path = require('path');
-require('./proxy');
-
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
+import initProxy from './proxy';
 import PTT from 'ptt-client';
 
-const ptt = new PTT({
-  url: 'ws://127.0.0.1:9770',
-});
+import * as path from 'path';
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-let firstTime = true;
+let proxyServer;
+let proxyAddress;
+
+async function intializeProxy () {
+  const { server, address } = await initProxy();
+  proxyServer = server;
+  proxyAddress = address;
+}
+
+function intializePttClient (url: string) {
+  return new Promise(resolve => {
+    const ptt = new PTT({ url });
+    ptt.once('connect', () => resolve(ptt));
+  });
+}
+
+let ptt;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  if (!proxyServer) {
+    await intializeProxy();
+  }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "vscode-ptt" is now active!');
+  if (!ptt) {
+    ptt = await intializePttClient(proxyAddress);
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', async () => {
+    const { login } = ptt.state;
 
-    // The code you place here will be executed every time your command is executed
-    if (firstTime) {
-      // !FIXME: workaround for PTT not get initialized properly
-      await sleep(1000);
-      firstTime = false;
+    if (!login) {
+      await ptt.login(process.env.USERNAME, process.env.PASSWORD)
     }
 
-    const isLogin = await ptt.login(process.env.USERNAME, process.env.PASSWORD)
-    if (!isLogin) {
-      // TODO: handle this
-
-      return;
-    }
-
-    // get last 20 articles from specific board
     let articles = await ptt.getArticles('C_Chat');
     console.log(articles);
-    // // get the content of specific article
-    // let article = await ptt.getArticle('C_Chat', articles[articles.length-1].sn);
-    // console.log(article);
 
 		// Display a message box to the user
 		// vscode.window.showInformationMessage('Hello World!');
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,40 +1,69 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+import * as http from 'http';
+import * as httpProxy from 'http-proxy';
+import * as fp from 'find-free-port';
 
-const config = {
-  server: {
-    host: '127.0.0.1',
-    port: 9770,
-  },
-  silent: false,
-};
+const logger = console;
 
-const logging = config.silent ? (...args) => {} : console.log;
+interface IProxyConfig {
+  server: any;
+  silent: boolean;
+}
 
-const proxy = httpProxy.createProxyServer({
-  ws: true,
-  target: 'wss://ws.ptt.cc/bbs',
-  changeOrigin: true,
-  headers: {
-    origin: 'https://www.ptt.cc',
-  }
-});
-
-const proxyServer = http.createServer((req, res) => {
-  proxy.ws(req, res);
-}).listen(config.server);
-
-proxyServer
-  .on('upgrade', (req, socket, head) => {
-    const remoteAddress =
-      req.headers['x-forwarded-for'] ||
-      socket.remoteAddress;
-    logging(`connection from ${remoteAddress}`);
-    proxy.ws(req, socket, head);
-  })
-  .on('listening', () => {
-    logging(`listening on ${config.server.host}:${config.server.port}`);
-  })
-  .on('error', (err) => {
-    logging(err);
+function createProxy () {
+  const proxy = httpProxy.createProxyServer({
+    target: 'wss://ws.ptt.cc/bbs',
+    changeOrigin: true,
+    headers: {
+      origin: 'https://www.ptt.cc',
+    }
   });
+
+  return proxy;
+}
+
+function startProxyServer (proxy: httpProxy, config: IProxyConfig) {
+  const proxyServer = http.createServer((req, res) => {
+    proxy.ws(req, res, undefined);
+  }).listen(config.server);
+
+  // TODO: handle proxy error restart
+  proxyServer
+    .on('upgrade', (req, socket, head) => {
+      const remoteAddress =
+        req.headers['x-forwarded-for'] ||
+        socket.remoteAddress;
+      logger.log(`connection from ${remoteAddress}`);
+      proxy.ws(req, socket, head);
+    })
+    .on('listening', () => {
+      logger.log(`listening on ${config.server.host}:${config.server.port}`);
+    })
+    .on('error', (err) => {
+      logger.log(err);
+    });
+
+  return proxyServer;
+}
+
+
+export default async function () {
+  const [port] = await fp(9700, 9800);
+  const config: IProxyConfig = {
+    server: {
+      host: '127.0.0.1',
+      port
+    },
+    silent: false
+  };
+
+  const proxy = createProxy();
+  const server = startProxyServer(proxy, config);
+
+  const address = `ws://127.0.0.1:${port}`;
+
+  return {
+    server,
+    address
+  };
+}
+
